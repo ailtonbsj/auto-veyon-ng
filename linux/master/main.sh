5a95934a11ff9f8ad32e1e5c6b39accabc0890e3
ailtonbsj <ailton.ifce@gmail.com>
2024-04-23 15:24:05 -0300

    removing comments

--- linux/master/main.sh
+++ linux/master/main.sh
@@ -19,16 +19,10 @@ else
     apt install $debpkg -y
 fi
 
-#sed -i '/Exec=/c\Exec=sudo /usr/bin/veyon-master' /usr/share/applications/veyon-master.desktop
-#sed -i '/Terminal=false/c\Terminal=true' /usr/share/applications/veyon-master.desktop
-
 veyon-cli config import confs.json
 
 veyon-cli authkeys create lei
 veyon-cli authkeys export lei/public lei.pem
-# /etc/veyon/keys/public/lei/key
 
 veyon-cli networkobjects add location "LEI"
 echo "You must reboot your system!"
-
-# veyon-cli networkobjects add computer "PC 01" vm22wx.local 08:00:27:97:c5:8b "LEI"
